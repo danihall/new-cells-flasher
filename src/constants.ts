f2e4c25c8aad4134bf503db8e929dcdd3433753d
Daniel Hallé <danielhalle82@gmail.com>
2023-03-10 16:04:59 +0100

    update cells per row constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const MIN_CELLS_PER_ROW = 3;
-const MAX_CELLS_PER_ROW = 60;
+const MAX_CELLS_PER_ROW = 80;
 const COUNTDOWN_TIME = 5000;
 const STORAGE_NAME = "cells_flasher_registered_arts";
 const LAST_ART_NAME = "cells_flasher_last_art";
