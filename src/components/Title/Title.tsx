3844650f3df96c476a62151d2c18a4271d93f0fe
Daniel Hallé <danielhalle82@gmail.com>
2023-03-11 17:10:32 +0100

    update title css

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,11 +1,11 @@
 import { ITitleProps } from "../../custom_types/titles";
-import utilsCSS from "../../index.module.scss";
+import rootCss from "../../index.module.scss";
 
 import css from "./Title.module.scss";
 
 const Title = ({ children }: ITitleProps): JSX.Element => {
   return (
-    <h1 className={utilsCSS["margin-top"]}>
+    <h1 className={`${css.h1} ${rootCss["margin-top"]}`}>
       <span className={css["h1-text"]}>{children}</span>
     </h1>
   );
